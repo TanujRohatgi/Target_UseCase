20850293d98aa1e841269faa42899a14990f21c7
Tanuj Rohatgi <33033986+TanujRohatgi@users.noreply.github.com>
2018-07-13 13:31:10 +0530

    Add files via upload

---
+++ ResponseObject.java
@@ -0,0 +1,21 @@
+package com.target.response;
+
+public class ResponseObject {
+
+	Boolean isAllowed;
+	String response;
+	public Boolean getIsAllowed() {
+		return isAllowed;
+	}
+	public void setIsAllowed(Boolean isAllowed) {
+		this.isAllowed = isAllowed;
+	}
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+	
+}
